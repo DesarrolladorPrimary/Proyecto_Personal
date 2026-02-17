5f70961c1d8cac6d813ac7d8e0c57f6a57129c75
Keyner Arismendy <keinerf588@gmail.com>
2026-02-17 11:54:41 -0500

    Implementacion de eliminacion de usuario

--- src/scripts/pages/settings/setting-user.js
+++ src/scripts/pages/settings/setting-user.js
@@ -1,27 +1,39 @@
+import { dataToken } from '../../utils/dataToken.js'
+
+const button_delete = document.querySelector("#button-del")
+const modal_confirm = document.querySelector("modal-confirm")
+const button_confirm = document.querySelector("#confirm_delete");
+const modal_processing = document.querySelector("#modal-processing");
+const modal_success = document.querySelector("#modal-success");
 
 
-const deleteUser = async()=>{
-    fetch('http://localhost:8080/api/v1/usuarios/id')
+
+const deleteUser = async (id, token) => {
+    await fetch(`http://localhost:8080/api/v1/usuarios/id?id=${id}`, {
+        method: 'DELETE',
+        headers: {
+            Authorization: "Bearer " + token,
+        },
+
+    })
 }
-
-
-
-
-
 
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const button_confirm = document.querySelector("#confirm_delete");
-    const modal_processing = document.querySelector("#modal-processing");
-    const modal_success = document.querySelector("#modal-success");
+    let datos = dataToken();
 
 
-    button_confirm.addEventListener('click', ()=>{
-        deleteUser()
+    const { id } = datos
+
+    const token = localStorage.getItem("Token");
+
+    button_delete.addEventListener('click', ()=>{
+        modal_confirm.classList.add("modal--active")
     })
 
-    
+
+
 });
 
 
